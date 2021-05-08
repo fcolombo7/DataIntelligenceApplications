c8ebe070d857dbb53fcdf70aff2e75c7a1759507
fcolombo7 <colombofilippo.fc@gmail.com>
2021-05-08 10:12:45 +0200

    update aggregate n_clicks

--- data_generators/basic_generator.py
+++ data_generators/basic_generator.py
@@ -105,10 +105,10 @@ class BasicDataGenerator(DataGenerator):
         for cl in self._daily_clicks:
             clicks_per_bid = list(cl['upper_bound'] * (1.0 - np.exp(-1 * cl['speed_factor'] * np.array(self._bids))))
             daily_clicks.append(clicks_per_bid)
-        # TODO: CHECK HERE! devo mettere decimals = 0 poichè int? Forse no perchè questa è solo la media.
+        # TODO: CHECK HERE! Somma o media? Forse meglio somma.
         if mode == 'all':
             return np.around(daily_clicks, decimals=3)
-        return np.around(np.average(daily_clicks, axis=0, weights=self._class_distribution), decimals=3)
+        return np.around(np.sum(daily_clicks, axis=0), decimals=3)
 
     def get_costs_per_click(self, mode='all'):
         """
