c366513c954f8573ee860c483237618a2aa92eb9
leoguerra97 <48678154+leoguerra97@users.noreply.github.com>
2021-05-13 11:23:45 +0200

    Add files via upload

--- learners/GPTS_Learner.py
+++ learners/GPTS_Learner.py
@@ -16,11 +16,16 @@ class GPTS_Learner(Learner):
         self.negative_threshold = 0.2
         self.penalty = 0.8
         self.pulled_arms = []
+        
+        self.min_rew = -500
+        self.max_rew = 1000
+        
         alpha = 10.0
         kernel = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-3, 1e3))
         self.gp = GaussianProcessRegressor(kernel = kernel, alpha = alpha**2, normalize_y = True, n_restarts_optimizer = 9)
         
     def update_observations(self, arm_idx, reward):
+        norm_reward = (reward - self.min_rew)/(self.max_rew - self.min_rew) 
         super().update_observations(arm_idx, reward)
         self.ineligibility[arm_idx] = norm.cdf(0, self.means[arm_idx], self.sigmas[arm_idx])
         self.pulled_arms.append(self.arms[arm_idx])
@@ -40,9 +45,11 @@ class GPTS_Learner(Learner):
         
     def pull_arm(self):
         if self.t < 10:
-            return np.random.choice(self.n_arms)
+            arm = np.random.choice(self.n_arms)
+            print(f'arm = {arm}')
+            return arm
         penalized_means = self.means
         penalized_means[self.ineligibility > self.negative_threshold] = \
-            penalized_means[self.ineligibility > self.negative_threshold] * self.penalty
+                penalized_means[self.ineligibility > self.negative_threshold] * self.penalty
         sampled_values = np.random.normal(penalized_means, self.sigmas)
         return np.argmax(sampled_values)
